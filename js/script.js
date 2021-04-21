190ce05ea6ba346cd8f6d63d8ebdf64f1ea263e1
Beto Pereira <betosantanna@outlook.com>
2021-04-21 20:32:58 -0300

    Modal box working

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
-var question = document.querySelector(".question");
-var answer = document.querySelector(".answer");
+var ftroca = document.querySelector(".ftroca");
+var fpay = document.querySelector(".fpay");
+var closeModal = document.querySelector(".close-modal");
+var modalBox = document.querySelector(".modal");
+
+ftroca.addEventListener("click", function() {
+  modalBox.style.display = 'flex';
+});
+
+fpay.addEventListener("click", function() {
+  modalBox.style.display = 'flex';
+});
+
+closeModal.addEventListener("click", function() {
+  modalBox.style.display = 'none';
+});
