bdcfde12ee2369aa05deb4eea0b2bc1ce75b064e
Beto Pereira <betosantanna@outlook.com>
2021-05-10 18:45:55 -0300

    Start backend

---
+++ php/databaseconnect.php
@@ -0,0 +1,12 @@
+<?php
+  require_once 'pdoconfig.php';
+
+  try {
+    $conn = new
+    PDO ("mysql:host=$host;dbname=$dbname", $username,
+    $password);
+    echo "Conectado a $dbname em $host com sucesso.";
+  } catch (PDOException $pe) {
+    die("Não foi possível se conectar com o banco de dados $dbname :" . $pe->getMessage());
+  }
+?>
---
+++ php/pdoconfig.php
@@ -0,0 +1,6 @@
+<?php
+  $host = 'localhost';
+  $dbname = 'u653934600_gioiaprodutos';
+  $username = 'u653934600_gioia';
+  $password = '@Gioia2021';
+?>
